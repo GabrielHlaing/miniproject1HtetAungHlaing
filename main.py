519dc2b49d60f7e017a98f125001b72b9fdf901d
Gabriel <h_hlaing@mail.fhsu.edu>
2025-09-12 17:18:20 +0630

    Plots are created using matplotlib

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # (5/5 points) Proper import of packages used.
 # (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
-# (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+# (10/10 points) Store this information in a list that you will convert to an array in NumPy.
 # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
@@ -15,9 +15,10 @@
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
-import pprint
+import copy
 
-stockList = ['GME', 'CWD', 'HUM', 'AAPL', 'MSFT']
+
+stockList = ['ADBE', 'CRCL', 'HUM', 'AAPL', 'MSFT']
 stockData = {}
 
 for stock in stockList:
@@ -27,4 +28,13 @@ for stock in stockList:
     for price in last10['Close']:
         stockData[stock].append(price)
     stocksArray = np.array(stockData[stock])
-    pprint.pprint(stocksArray)
+
+    # Copy the array prices to a list to determine min and max prices
+    newList = copy.copy(stockData[stock])
+    newList.sort()
+
+    plt.plot(stocksArray)
+    plt.axis((0, 10, newList[0]-10, newList[-1]+10))
+    plt.xlabel('Last 10 Trading Days')
+    plt.ylabel('Closing Price')
+    plt.show()
